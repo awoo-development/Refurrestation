8103154f09ea13cd3be12ca97db8fb28ef1bcaa9
Pooka The Wolf <mystikalpooka@gmail.com>
2019-03-29 21:15:10 -0700

    derpfoxxo player camera addition

---
+++ Assets/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class CameraController : MonoBehaviour
+{
+    public GameObject Player;
+    public Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - Player.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = Player.transform.position + offset;
+
+    }
+}
---
+++ Assets/PlayerRotation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRotation : MonoBehaviour
+{
+    public Transform CharacterTransform;
+    public float RotationSmoothingCoef = 0.01f;
+
+    private Quaternion targetRotation;
+
+    void Update()
+    {
+        var groundPlane = new Plane(Vector3.up, -CharacterTransform.position.y);
+        var mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float hitDistance;
+
+        if (groundPlane.Raycast(mouseRay, out hitDistance))
+        {
+            var lookAtPosition = mouseRay.GetPoint(hitDistance);
+            targetRotation = Quaternion.LookRotation(lookAtPosition - CharacterTransform.position, Vector3.up);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        var rotation = Quaternion.Lerp(CharacterTransform.rotation, targetRotation, RotationSmoothingCoef);
+        CharacterTransform.rotation = rotation;
+    }
+}
